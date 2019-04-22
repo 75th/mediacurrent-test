69810d37835211140cb575a42f0a55ee219194ee
Lanny Heidbreder <lanny@75thtrombone.com>
2019-04-22 11:19:16 -0500

    Shuffling files around, beginning CSS and JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-window.addEventListener('DOMContentLoaded')
+var $ = document.querySelector.bind(document);
+
+(function() {
+	var $body = $('body');
+	console.log($body);
+
+	$body.classList.add('js');
+
+	$('button.to-dark').addEventListener('click', function() {
+		$body.classList.remove('light');
+		$body.classList.add('dark');
+	});
+
+	$('button.to-light').addEventListener('click', function() {
+		$body.classList.remove('dark');
+		$body.classList.add('light');
+	});
+})();
