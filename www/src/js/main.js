104df95ae1b202da42d3566f043d70b03d67563c
Lanny Heidbreder <lanny@75thtrombone.com>
2019-04-23 09:16:13 -0500

    More

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -2,17 +2,38 @@ var $ = document.querySelector.bind(document);
 
 (function() {
 	var $body = $('body');
-	console.log($body);
 
 	$body.classList.add('js');
 
+	const modeShift = function(toDark) {
+		// Transition duration is saved as a CSS custom property on the body element
+		var transitionDuration = getComputedStyle($body).getPropertyValue('--transition-duration');
+		const factor = transitionDuration.indexOf('ms') !== -1 ? 1 : 1000;
+		transitionDuration = parseInt(transitionDuration) * factor;
+
+		if(typeof toDark === 'undefined') {
+			toDark = true;
+		}
+
+		// We don't want transitions on hover, but we do want them on mode change,
+		// so here's a temporary class to enable transition durations
+		$body.classList.add('mode-shifting')
+		$body.classList.remove(toDark ? 'light' : 'dark');
+		$body.classList.add(toDark ? 'dark' : 'light');
+		setTimeout(
+			function() { $body.classList.remove('mode-shifting') },
+			transitionDuration
+		);
+	};
+
 	$('button.to-dark').addEventListener('click', function() {
-		$body.classList.remove('light');
-		$body.classList.add('dark');
+		modeShift(true);
 	});
 
 	$('button.to-light').addEventListener('click', function() {
-		$body.classList.remove('dark');
-		$body.classList.add('light');
+		modeShift(false);
 	});
+
+	var darkModeMQ = matchMedia('(prefers-color-scheme: dark)');
+	$body.addClass(darkModeMQ.matches ? 'dark' : 'light');
 })();
