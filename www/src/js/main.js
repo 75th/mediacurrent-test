99a54e3cdf3b5aa31209411346262c7737caf1eb
Lanny Heidbreder <lanny@75thtrombone.com>
2019-04-23 16:01:17 -0500

    Lots of JS work, including responding to OS color mode changes and remembering the user's last manual choice

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -1,40 +1,60 @@
 var $ = document.querySelector.bind(document);
 var $$ = document.querySelectorAll.bind(document);
 
-(function() {
+(() => {
 	var $body = $('body');
-
 	$body.classList.add('js');
 
-	const modeShift = (toDark = true) => {
-		// Transition duration is saved as a CSS custom property on the body element
-		var transitionDuration = getComputedStyle($body).getPropertyValue('--transition-duration');
-		const factor = transitionDuration.indexOf('ms') !== -1 ? 1 : 1000;
-		transitionDuration = parseInt(transitionDuration) * factor;
+	var manualColorMode = false;
+
+	var colorModeSwitch = (toDark = true, manualSwitch = true, transition = manualSwitch) => {
+		var transitionDuration = transition ? 500 : 0;
+		var transitionDurationCSS = `${transitionDuration}ms`;
+
+		if(manualSwitch) {
+			manualColorMode = true;
+			localStorage.setItem('prefersDark', toDark);
+		}
 
 		// Disable mode buttons during transition
 		const $$buttons = $$('.color-modes button');
-	//	$$buttons.forEach(($button) => { $button.disabled = true });
+		$$buttons.forEach(($button) => { $button.disabled = true });
 
-		// We don't want transitions on hover, but we do want them on mode change,
-		// so here's a temporary class to enable transition durations
+		$body.style.setProperty('--transition-duration', transitionDurationCSS);
 		$body.classList.add('mode-shifting');
 
 		$body.classList.remove(toDark ? 'light' : 'dark');
 		$body.classList.add(toDark ? 'dark' : 'light');
+
 		setTimeout(
 			() => {
-				$body.classList.remove('mode-shifting')
-			//	$$buttons.forEach(($button) => { $button.disabled = false });
+				$body.classList.remove('mode-shifting');
+				$body.style.setProperty('--transition-duration', '0s');
+				$$buttons.forEach(($button) => { $button.disabled = false });
 			},
 			transitionDuration
 		);
 	};
 
-	$('button.to-dark').addEventListener('click', () => { modeShift(true); });
+	$('button.to-dark').addEventListener('click', () => { colorModeSwitch(true); });
+	$('button.to-light').addEventListener('click', () => { colorModeSwitch(false); });
 
-	$('button.to-light').addEventListener('click', () => { modeShift(false); });
 
-	var darkModeMQ = matchMedia('(prefers-color-scheme: dark)');
-	$body.classList.add(darkModeMQ.matches ? 'dark' : 'light');
+	var prefersDark = localStorage.getItem('prefersDark');
+	prefersDark = prefersDark === 'false' ? false : prefersDark === 'true' ? true : null;
+
+	// Remember the last manual selection foreeeeeevvverrrrr
+	if(prefersDark !== null) {
+		colorModeSwitch(prefersDark, false);
+	} else {
+		// If no manual selection, use OS settings
+		const darkModeMQ = matchMedia('(prefers-color-scheme: dark)');
+		colorModeSwitch(darkModeMQ.matches, false);
+
+		darkModeMQ.addListener((e) => {
+			if(!manualColorMode) {
+				colorModeSwitch(e.matches, false, true);
+			}
+		});
+	}
 })();
