4418a83df5b58879436ebd76f05ef2a97b7a16d2
Lanny Heidbreder <lanny@75thtrombone.com>
2019-04-23 13:36:59 -0500

    Color mode switching working, and now using CSS custom properties

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -1,39 +1,40 @@
 var $ = document.querySelector.bind(document);
+var $$ = document.querySelectorAll.bind(document);
 
 (function() {
 	var $body = $('body');
 
 	$body.classList.add('js');
 
-	const modeShift = function(toDark) {
+	const modeShift = (toDark = true) => {
 		// Transition duration is saved as a CSS custom property on the body element
 		var transitionDuration = getComputedStyle($body).getPropertyValue('--transition-duration');
 		const factor = transitionDuration.indexOf('ms') !== -1 ? 1 : 1000;
 		transitionDuration = parseInt(transitionDuration) * factor;
 
-		if(typeof toDark === 'undefined') {
-			toDark = true;
-		}
+		// Disable mode buttons during transition
+		const $$buttons = $$('.color-modes button');
+	//	$$buttons.forEach(($button) => { $button.disabled = true });
 
 		// We don't want transitions on hover, but we do want them on mode change,
 		// so here's a temporary class to enable transition durations
-		$body.classList.add('mode-shifting')
+		$body.classList.add('mode-shifting');
+
 		$body.classList.remove(toDark ? 'light' : 'dark');
 		$body.classList.add(toDark ? 'dark' : 'light');
 		setTimeout(
-			function() { $body.classList.remove('mode-shifting') },
+			() => {
+				$body.classList.remove('mode-shifting')
+			//	$$buttons.forEach(($button) => { $button.disabled = false });
+			},
 			transitionDuration
 		);
 	};
 
-	$('button.to-dark').addEventListener('click', function() {
-		modeShift(true);
-	});
+	$('button.to-dark').addEventListener('click', () => { modeShift(true); });
 
-	$('button.to-light').addEventListener('click', function() {
-		modeShift(false);
-	});
+	$('button.to-light').addEventListener('click', () => { modeShift(false); });
 
 	var darkModeMQ = matchMedia('(prefers-color-scheme: dark)');
-	$body.addClass(darkModeMQ.matches ? 'dark' : 'light');
+	$body.classList.add(darkModeMQ.matches ? 'dark' : 'light');
 })();
