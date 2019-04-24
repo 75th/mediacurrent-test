3a3b9f7538ed74566c97b315e72f1fb0be3d80d4
Lanny Heidbreder <lanny@75thtrombone.com>
2019-04-24 11:58:47 -0500

    More comments in JS

--- www/src/js/main.js
+++ www/src/js/main.js
@@ -6,7 +6,7 @@ var $$ = document.querySelectorAll.bind(document);
 
 	var colorIcon = '<svg height="30" viewBox="0 0 30 30" width="30" xmlns="http://www.w3.org/2000/svg"><path d="m15 0a15 15 0 1 0 15 15 15 15 0 0 0 -15-15zm0 1.76a13.24 13.24 0 0 1 0 26.48z" fill="#003954"/></svg>';
 
-	// Add buttons
+	// Add color mode buttons
 	$('div.color-modes').innerHTML =
 		`<button class="to-dark">${colorIcon}<span>Dark Mode</button><button class="to-light">${colorIcon}<span>Light Mode</span></button>`
 	;
@@ -15,6 +15,15 @@ var $$ = document.querySelectorAll.bind(document);
 	// so we can stop monitoring the OS setting in the middle of a session.
 	var manualColorMode = false;
 
+	/**
+	 * Handle all aspects of switching color modes.
+	 *
+	 * @param {bool} toDark - If true, switch to Dark Mode; if false, Light Mode.
+	 * @param {bool} manualSwitch - If true, the user manually chose a mode by clicking; if false, we're switching for another reason.
+	 * @param {bool} transition - Whether or not to use a transition duration for this switch
+	 *
+	 * @return {undefined}
+	 */
 	var colorModeSwitch = (toDark = true, manualSwitch = true, transition = manualSwitch) => {
 		const transitionDuration = transition ? 500 : 0;
 		const transitionDurationCSS = `${transitionDuration}ms`;
@@ -28,12 +37,15 @@ var $$ = document.querySelectorAll.bind(document);
 		const $$buttons = $$('.color-modes button');
 		$$buttons.forEach(($button) => { $button.disabled = true });
 
+		// Temporarily adjust transition duration
 		$body.style.setProperty('--transition-duration', transitionDurationCSS);
-		$body.classList.add('mode-shifting');
 
+		// Toggle classes
+		$body.classList.add('mode-shifting');
 		$body.classList.remove(toDark ? 'light' : 'dark');
 		$body.classList.add(toDark ? 'dark' : 'light');
 
+		// Clean up after switch completes
 		setTimeout(
 			() => {
 				$body.classList.remove('mode-shifting');
@@ -44,20 +56,25 @@ var $$ = document.querySelectorAll.bind(document);
 		);
 	};
 
+	// Attach listeners to color mode buttons
 	$('button.to-dark').addEventListener('click', () => { colorModeSwitch(true); });
 	$('button.to-light').addEventListener('click', () => { colorModeSwitch(false); });
 
+	// Fetch user's manual color preference from storage
 	var prefersDark = localStorage.getItem('prefersDark');
+	// Local storage only stores strings, not booleans
 	prefersDark = prefersDark === 'false' ? false : prefersDark === 'true' ? true : null;
 
-	// Remember the last manual selection foreeeeeevvverrrrr
+	// Apply user's manual preference if one exists
 	if(prefersDark !== null) {
 		colorModeSwitch(prefersDark, false);
 	} else {
-		// If no manual selection, use OS settings
+		// If no manual preference, use OS settings
 		const darkModeMQ = matchMedia('(prefers-color-scheme: dark)');
 		colorModeSwitch(darkModeMQ.matches, false);
 
+		// Respond to changes in OS setting, unless the user has manually
+		// selected a mode during this page load
 		darkModeMQ.addListener((e) => {
 			if(!manualColorMode) {
 				colorModeSwitch(e.matches, false, true);
